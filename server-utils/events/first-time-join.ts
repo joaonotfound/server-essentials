51dbc59582469d0327e19237b6e465802a2c9d5f
joaonotfound <joaonotfound@protonmail.com>
2023-06-17 14:32:54 -0300

    feat: uncomment some code

--- server-utils/events/first-time-join.ts
+++ server-utils/events/first-time-join.ts
@@ -1,21 +1,22 @@
 import { events } from "bdsx/event";
 import { savePlayerStorage, openPlayerStorage } from "../../utils";
 import { createPlot } from "../utils/create-plot";
+import { bedrockServer } from "bdsx/launcher";
 
-// events.playerJoin.on(async ({ player }) => {
-//     const storage = await openPlayerStorage(player)
-//     if (!storage.home) {
-//         const plot = await createPlot(player)
-//         storage.home = plot.key
-//         savePlayerStorage(player, storage)
-//     }
-// })
+events.playerJoin.on(async ({ player }) => {
+    const storage = await openPlayerStorage(player)
+    if (!storage.home) {
+        const plot = await createPlot(player)
+        storage.home = plot.key
+        savePlayerStorage(player, storage)
+    }
+})
 
-// events.playerJoin.on(async ({ player }) => {
-//     const storage = await openPlayerStorage(player)
-//     if(storage.first_join){
-//         bedrockServer.executeCommand(`give "${player.getNameTag()}" stone_pickaxe 1 0 {"minecraft:can_destroy":{"blocks":["stone", "iron_ore", "lapis_ore", "copper_ore"]}}"`)
-//         storage.first_join = false
-//         savePlayerStorage(player, storage)
-//     }
-// })
+events.playerJoin.on(async ({ player }) => {
+    const storage = await openPlayerStorage(player)
+    if(storage.first_join){
+        bedrockServer.executeCommand(`give "${player.getNameTag()}" stone_pickaxe 1 0 {"minecraft:can_destroy":{"blocks":["stone", "iron_ore", "lapis_ore", "copper_ore"]}}"`)
+        storage.first_join = false
+        savePlayerStorage(player, storage)
+    }
+})
